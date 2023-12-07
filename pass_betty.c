025f08e53bf04ba28dc136f59a852bebf5974a2b
Onwuemebolam Darlington Kelechi <darlinobest@yahoo.com>
2023-12-06 20:15:47 -0800

    simple code which passes betty checks

---
+++ pass_betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints message that passes betty checks
+ * Return: 0 upon success
+ */
+
+int main(void)
+{
+	printf("I passed betty check\n");
+	return (0);
+}
